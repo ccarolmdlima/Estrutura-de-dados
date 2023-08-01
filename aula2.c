4ce8344cd0739d27b9bfd021de645c66eedff727
carol <126472626+ccarolmdlima@users.noreply.github.com>
2023-08-01 08:31:45 -0300

    Update aula2.c

--- aula2.c
+++ aula2.c
@@ -1,1 +1,31 @@
+//////////////////////////////////
+// programa de ordenacao direta //
+//////////////////////////////////
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/////////////////////////////
+// declaracao de variaveis //
+/////////////////////////////
+
+int vetor[10000];
+int i = 0;
+int j = 0;
+
+//////////////////////////////////
+// funcao principal de execucao //
+//////////////////////////////////
+
+int main() {
+	
+	// inicializacao do vetor
+	for (i = 0; i < 10000; i++) {
+		vetor[i] = (rand() % 10000);
+	}
+	
+	// impressao do vetor
+	for (i = 0; i < 10000; i++) {
+		printf("%d\n", vetor[i]);
+	}
+}
