30d05602fc34451a7c177b1585073699b63f0b61
carol <126472626+ccarolmdlima@users.noreply.github.com>
2023-08-08 08:49:42 -0300

    Update aula3listaencadeadasimples.c

--- aula3listaencadeadasimples.c
+++ aula3listaencadeadasimples.c
@@ -1,1 +1,35 @@
+/////////////////////////////////////////
+// Programa de Lista Encadeada simples //
+////////////////////////////////////////
 
+#include <stdio.h>
+#include <stdlib.h>
+
+//////////////////////////////////
+// Definição da Estrutura do No //
+/////////////////////////////////
+
+struct no {
+	int numero;
+	struct no *proximo;
+};
+
+/////////////////////////////
+// Declaração de variaveis //
+////////////////////////////
+
+struct no *cabeca;
+
+//////////////////////
+// Função principal //
+/////////////////////
+
+int main () {
+	cabeca = (struct no *) malloc(sizeof(struct no *));
+	cabeca -> numero = 10;
+	cabeca -> proximo = NULL;
+	
+	printf("%s\n", cabeca);
+	printf("%d\n", cabeca -> numero);
+	
+}
