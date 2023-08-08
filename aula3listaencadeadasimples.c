a08ac9aac11eb92155bfe8b9e3076ea720c09c19
carol <126472626+ccarolmdlima@users.noreply.github.com>
2023-08-08 09:29:24 -0300

    Update aula3listaencadeadasimples.c

--- aula3listaencadeadasimples.c
+++ aula3listaencadeadasimples.c
@@ -99,3 +99,82 @@ int main () {
 	printf("%d\n", cabeca -> numero);
 	
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////
+// programa de Lista encadeada simples //
+////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//////////////////////////////////
+// definição da estrutura do no //
+/////////////////////////////////
+
+struct no {
+	int numero;
+	struct no *proximo;
+};
+
+/////////////////////////////
+// declaração de variaveis //
+////////////////////////////
+
+struct no *cabeca;
+
+//////////////////////////////////////
+// funcao que insere um no na lista //
+//////////////////////////////////////
+
+void inserir(int parametroNumero) {
+	
+	// criar um novo no
+	struct no *novoNo = NULL;
+	novoNo = (struct no *) malloc(sizeof(struct no*));
+	novoNo -> numero = parametroNumero;
+	novoNo -> proximo = NULL;
+			
+	if (cabeca == NULL) {
+		cabeca = novoNo;
+	} else {
+		// procurando o ultimo elemento da lista
+		struct no *ponteiro = cabeca;
+		while (ponteiro -> proximo != NULL) {
+			ponteiro = ponteiro -> proximo;
+		}
+		
+		// apontar o ultimo para o novo
+		ponteiro -> proximo = novoNo;
+	}
+}
+
+//////////////////////////////////////////////
+// funcao que imprime os elementos da lista //
+//////////////////////////////////////////////
+
+void imprimir() {
+	struct no *ponteiro = cabeca;
+	
+	while (ponteiro != NULL) {
+		printf("%d\n", ponteiro -> numero);
+		ponteiro = ponteiro -> proximo;
+	}
+}
+
+//////////////////////
+// funcao principal //
+/////////////////////
+
+int main () {
+	inserir(1);
+	inserir(2);
+	inserir(3);
+	inserir(4);
+	inserir(5);
+	inserir(6);
+	
+	imprimir();
+	
+}
