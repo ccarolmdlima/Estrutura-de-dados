56a06a340a022e96838aeb23c682bd67f168de6a
carol <126472626+ccarolmdlima@users.noreply.github.com>
2023-09-19 09:06:22 -0300

    Create aula08arvorebinaria.c

---
+++ aula08arvorebinaria.c
@@ -0,0 +1,38 @@
+/////////////////////////////////////////////////////////////
+// programa que implementa as navegacoes em arvore binaria //
+/////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//////////////////////////////
+// declaracao de estruturas //
+//////////////////////////////
+
+struct no {
+	int numero;
+	struct no *esquerda;
+	struct no *direita;
+};
+
+///////////////////////////////////////
+// funcao que insere um no na arvore //
+///////////////////////////////////////
+
+struct no *inserir (struct no *raiz, int numero) {
+	if (raiz == NULL) {
+		raiz = (struct no *) malloc(sizeof(struct no));
+		raiz -> numero = numero;
+		raiz -> esquerda = NULL;
+		raiz -> direita = NULL;
+	} else {
+		if ((rand() % 2) == 0) {
+			raiz -> esquerda = inserir(raiz -> esquerda, numero);
+		} else {
+			raiz -> direita = inserir(raiz -> direita, numero);
+		}
+	}
+	
+	return raiz;
+}
